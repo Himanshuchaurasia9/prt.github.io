0c4165a7b97324ad288ff4792e6b26c8a93b9612
Himanshu Chaurasia <106426822+Himanshuchaurasia9@users.noreply.github.com>
2023-07-25 13:16:01 +0530

    PRT Website

---
+++ assets/js/main.js
@@ -0,0 +1,18 @@
+const section = document.querySelectorAll("section");
+const links = document.querySelectorAll("a");
+
+window.onscroll = ()=>{
+    section.forEach(sec =>{
+        let top = window.scrollY;
+        let offset = sec.offsetTop-150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute("id");
+
+        if(top >= offset && top < offset + height){
+            links.forEach(link =>{
+                link.classList.remove("active");
+                document.querySelector("header nav ul li a[href*="+ id +"]").classList.add("active");
+            })
+        }
+    })
+}
